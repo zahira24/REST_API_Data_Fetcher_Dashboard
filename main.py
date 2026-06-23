2b15c3980b7d51b04462a390fce294f69076e36a
Zahira Shirin <zahirashirin64@gmail.com>
2026-06-23 16:57:33 +0530

    Final Expense Tracker with Analytics and Visualization

---
+++ main.py
@@ -0,0 +1,8 @@
+from tracker import view_expenses, total_expense, category_expense, category_pie_chart
+
+print("\n--- EXPENSE TRACKER DASHBOARD ---\n")
+
+view_expenses()
+total_expense()
+category_expense()
+category_pie_chart()
